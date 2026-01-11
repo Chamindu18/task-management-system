3beb92f17b397fef11c97707f75aa76102258f40
Amandi1020 <amandirajapaksha2003@gmail.com>
2026-01-11 23:35:21 +0530

    Add TaskList UI component

---
+++ frontend/src/components/tasks/TaskList.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import TaskCard from './TaskCard';
+import { FaSpinner, FaInbox } from 'react-icons/fa';
+
+const TaskList = ({ tasks, loading, onEdit, onDelete, pagination, onPageChange }) => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
