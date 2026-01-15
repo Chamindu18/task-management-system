6cea4c8a1f2d33cd3b8b3e1bca8436d02cd0e5af
Chamindu18 <rachranasinghe@students.nsbm.ac.lk>
2026-01-15 16:17:33 +0530

    adminuser dto error fixed

--- backend/src/main/java/org/task_manager/backend/dto/AdminUserDto.java
+++ backend/src/main/java/org/task_manager/backend/dto/AdminUserDto.java
@@ -1,12 +1,10 @@
 package org.task_manager.backend.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
-@AllArgsConstructor
 public class AdminUserDto {
     private Long id;
     private String username;
