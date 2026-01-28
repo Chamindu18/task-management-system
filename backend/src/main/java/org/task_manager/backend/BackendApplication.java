3f3bd0cb560c3dda6d0a226b22e7d29bc1d9696e
Chamindu18 <rachranasinghe@students.nsbm.ac.lk>
2026-01-28 22:00:40 +0530

    admin dashboard task priority distribution chart erros fixed

--- backend/src/main/java/org/task_manager/backend/BackendApplication.java
+++ backend/src/main/java/org/task_manager/backend/BackendApplication.java
@@ -2,10 +2,14 @@ package org.task_manager.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@EntityScan(basePackages = "org.task_manager.backend.model")
+@EnableJpaRepositories(basePackages = "org.task_manager.backend.repository")
 public class BackendApplication {
 
     public static void main(String[] args) {
